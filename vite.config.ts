cc4d659177863f24d46c3ce534a3a38152458725
garrettmills <shout@garrettmills.dev>
2022-04-09 13:02:30 -0500

    Add proxy for api

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import vuetify from '@vuetify/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8000/',
+            },
+        },
+    },
     plugins: [vue(),
         vuetify({ autoImport: true }), // Enabled by default
     ],
