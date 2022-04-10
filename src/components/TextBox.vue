3071947c281c5647af0b4ffb92f3774829cd3530
Thomas <dev.csnova@gmail.com>
2022-04-09 19:03:19 -0500

    removed abs positioning

--- src/components/TextBox.vue
+++ src/components/TextBox.vue
@@ -14,7 +14,7 @@ const props = defineProps({
     :grid="[stepX, stepY]"
     :default-position="{ x: props.value.x, y: props.value.y }"
   >
-    <div style="display: flex; bottom: 10px; right: 0">
+    <div style="display: flex;">
       <q-card flat bordered>
         <q-card-section style="padding: 0">
           <div class="row items-center no-wrap">
